c0f5eb090f9dd2f47e1114cbcfbdaf393bf8e432
Steve Min <stevekmin@gmail.com>
2020-01-28 19:13:21 +0000

    python variable practice

--- Practice/app.py
+++ Practice/app.py
@@ -1,6 +1,5 @@
-print("     /|")
-print("    / |")
-print("   /  |")
-print("  /   |")
-print(" /    |")
-print("/_____|")
+character_name = 'Steve'
+character_age = '35'
+print('There once was a man named ' + character_name)
+print('he was 35 years ' + character_age)
+print('he really like the name ' + character_name)
