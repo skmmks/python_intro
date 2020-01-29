d680e45f58083d30bcd5681dec6c85c93a4036f5
Steve Min <stevekmin@gmail.com>
2020-01-29 16:56:32 +0000

    add , to allow for concatonation of str and int

--- Practice/app.py
+++ Practice/app.py
@@ -2,6 +2,6 @@ character_name = 'Steve'
 character_age = 35
 is_male = True
 print('There once was a man named ' + character_name)
-print('he was 35 years ' + character_age)
+print('he was 35 years ', character_age)
 character_name = 'New Steve'
 print('he really like the name ' + character_name)
