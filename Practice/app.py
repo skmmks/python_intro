5a6e5056ed34505899833daf8276184a5b3de076
Steve Min <stevekmin@gmail.com>
2020-01-29 16:51:55 +0000

    redefine variable practice

--- Practice/app.py
+++ Practice/app.py
@@ -2,4 +2,5 @@ character_name = 'Steve'
 character_age = '35'
 print('There once was a man named ' + character_name)
 print('he was 35 years ' + character_age)
+character_name = 'New Steve'
 print('he really like the name ' + character_name)
