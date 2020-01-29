c5101cf1ae216e36efecc498785b363de70af9b0
Steve Min <stevekmin@gmail.com>
2020-01-29 16:54:34 +0000

    modify age string to number define boolean variable

--- Practice/app.py
+++ Practice/app.py
@@ -1,5 +1,6 @@
 character_name = 'Steve'
-character_age = '35'
+character_age = 35
+is_male = True
 print('There once was a man named ' + character_name)
 print('he was 35 years ' + character_age)
 character_name = 'New Steve'
